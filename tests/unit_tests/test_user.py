d30402a8b3dcc9449a96a78bdc89a36b3e57f9e3
daidai <dai.tamae@gmail.com>
2022-06-05 14:05:43 -0300

    Added github workflow for tests

--- tests/unit_tests/test_user.py
+++ tests/unit_tests/test_user.py
@@ -7,7 +7,7 @@ inject_dependencies()
 client = TestClient(create_app())
 
 
-def test_get_user_by_id():
+def test_get_user_by_id_success():
     response = client.get("/v1/users/507f1f77bcf86cd799439011")
     assert response.status_code == 200
     assert response.json() == {
@@ -15,3 +15,15 @@ def test_get_user_by_id():
         "email": "test_email",
         "username": "test_username",
     }
+
+
+def test_get_user_by_id_invalid_id():
+    response = client.get("/v1/users/invalid_id")
+    assert response.status_code == 400
+    assert response.json() == {"detail": "user id is not valid"}
+
+
+def test_get_user_by_id_not_found():
+    response = client.get("/v1/users/507f1f77bcf86cd799439021")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "user not found"}
