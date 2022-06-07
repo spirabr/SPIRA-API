cbabfdc75013501a381fd1d5632f677499527a1c
daidai <dai.tamae@gmail.com>
2022-06-07 15:46:08 -0300

    Added tests for models endpoints

--- tests/mocks/mongo_mock.py
+++ tests/mocks/mongo_mock.py
@@ -11,6 +11,8 @@ class MongoMock(MongoAdapter):
         self._conn = MongoClient()
         self._db = self._conn.spira_db
         self._users = self._db.users
+        self._models = self._db.models
+
         self._users.insert_one(
             {
                 "_id": ObjectId("507f191e810c19729de860ea"),
@@ -18,4 +20,21 @@ class MongoMock(MongoAdapter):
                 "email": "test_email",
                 "hashed_password": self.pwd_context.hash("fake_password"),
             }
+        )
+
+        self._models.insert_many(
+            [
+                {
+                    "_id": ObjectId("629f992d45cda830033cf4cd"),
+                    "name": "fake_model",
+                    "subscribing_topic": "fake_topic_1",
+                    "publishing_topic": "fake_topic_2",
+                },
+                {
+                    "_id": ObjectId("629f994245cda830033cf4cf"),
+                    "name": "fake_model_2",
+                    "subscribing_topic": "fake_topic_3",
+                    "publishing_topic": "fake_topic_4",
+                },
+            ]
         )
