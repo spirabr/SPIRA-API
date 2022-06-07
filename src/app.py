8ff20cde9016959962315619bbc30e2e26f1f2f7
daidai <dai.tamae@gmail.com>
2022-06-07 19:42:49 -0300

    Removed dependency injection, passing instances in router method arguments

--- src/app.py
+++ src/app.py
@@ -1,19 +1,21 @@
-from doctest import FAIL_FAST
 from fastapi import FastAPI
 import uvicorn
 
-from configuration import inject_dependencies
 from routers.v1.user_router import create_user_router
+
+from domain.ports.database_port import DatabasePort
+
+from adapters.database.mongo import MongoAdapter
 
 
 def create_app() -> FastAPI:
     app: FastAPI = FastAPI()
-    app.include_router(create_user_router())
+    database_port = DatabasePort(MongoAdapter())
+    app.include_router(create_user_router(database_port))
     return app
 
 
 if __name__ == "__main__":
-    inject_dependencies()
     uvicorn.run(
         create_app(),
         host="0.0.0.0",
--- src/domain/ports/database_port.py
+++ src/domain/ports/database_port.py
@@ -1,4 +1,3 @@
-import inject
 from typing import Union
 
 from domain.model.user import User, UserForm
@@ -6,9 +5,8 @@ from domain.interfaces.database_interface import DatabaseInterface
 
 
 class DatabasePort:
-    @inject.autoparams()
-    def __init__(self, database: DatabaseInterface):
-        self._database = database
+    def __init__(self, adapter: DatabaseInterface):
+        self._database = adapter
 
     def get_user_by_id(self, user_id: str) -> Union[User, None]:
         return self._database.get_user_by_id(user_id)
