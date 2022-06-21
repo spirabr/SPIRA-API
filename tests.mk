b346941b97f72415daf904127dc11875d77c4269
daidai <dai.tamae@gmail.com>
2022-06-21 13:47:52 -0300

    Separated tests per hierarchy Centralized test calls in tests.mk

---
+++ tests.mk
@@ -0,0 +1,9 @@
+make:
+	@echo running unit tests for adapters
+	python3 -m py.test tests/unit_tests/adapters
+
+	@echo running unit tests for ports
+	python3 -m py.test tests/unit_tests/ports
+
+	@echo running integration tests for endpoints
+	python3 -m py.test tests/integration_tests
