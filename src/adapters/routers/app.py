f5c0e3b35d1b907efefdb7f7e9a749c571ef5ae5
daidai <dai.tamae@gmail.com>
2022-07-15 12:34:39 -0300

    Changed ports dictionary to Ports class

--- src/adapters/routers/app.py
+++ src/adapters/routers/app.py
@@ -6,35 +6,36 @@ from adapters.routers.v1.result_router import create_result_router
 from adapters.routers.v1.inference_router import create_inference_router
 from adapters.routers.v1.model_router import create_model_router
 from adapters.routers.v1.user_router import create_user_router
+from core.ports.ports import Ports
 
 
-def create_app(ports: dict) -> FastAPI:
+def create_app(ports: Ports) -> FastAPI:
     app: FastAPI = FastAPI()
 
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
     app.include_router(
         create_inference_router(
-            ports["simple_storage_port"],
-            ports["message_service_port"],
-            ports["authentication_port"],
-            ports["database_port"],
+            ports.simple_storage_port,
+            ports.message_service_port,
+            ports.authentication_port,
+            ports.database_port,
             oauth2_scheme,
         )
     )
     app.include_router(
         create_model_router(
-            ports["authentication_port"], ports["database_port"], oauth2_scheme
+            ports.authentication_port, ports.database_port, oauth2_scheme
         )
     )
     app.include_router(
         create_user_router(
-            ports["authentication_port"], ports["database_port"], oauth2_scheme
+            ports.authentication_port, ports.database_port, oauth2_scheme
         )
     )
     app.include_router(
         create_result_router(
-            ports["authentication_port"], ports["database_port"], oauth2_scheme
+            ports.authentication_port, ports.database_port, oauth2_scheme
         )
     )
     return app
