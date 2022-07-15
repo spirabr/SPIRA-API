5932358a9c46c357c5f43e23882ff7c6d92022ba
daidai <dai.tamae@gmail.com>
2022-07-14 22:38:08 -0300

    Added unit tests to message service port

---
+++ tests/unit_tests/ports/test_message_service_port.py
@@ -0,0 +1,95 @@
+import json
+from mock import ANY, MagicMock, call, patch
+from pydantic import BaseModel
+from adapters.message_service.nats_adapter import NATSAdapter
+from core.model.constants import Status
+from core.model.inference import InferenceCreation
+from core.model.message_service import RequestLetter
+from core.model.result import ResultUpdate
+from core.ports.message_service_port import MessageServicePort
+from tests.mocks.nats_mock import NATSMock
+import pytest
+import asyncio
+
+adapter_instance = NATSMock()
+
+
+@pytest.fixture()
+def message_service_port():
+    port = MessageServicePort(adapter_instance)
+    return port
+
+
+def test_send_message(message_service_port: MessageServicePort):
+    async def fake_send_message(topic: str, message: bytes):
+        pass
+
+    with patch.object(
+        adapter_instance,
+        "send_message",
+        MagicMock(side_effect=fake_send_message),
+    ) as mock_method:
+        asyncio.run(
+            message_service_port.send_message(
+                RequestLetter(
+                    content=InferenceCreation(
+                        age=30,
+                        sex="M",
+                        model_id="fake_model_id",
+                        status=Status.processing_status,
+                        user_id="fake_user_id",
+                    ),
+                    publishing_channel="fake_topic",
+                ),
+            )
+        )
+        mock_method.assert_called_once_with(
+            json.dumps(
+                {
+                    "age": 30,
+                    "sex": "M",
+                    "model_id": "fake_model_id",
+                    "status": Status.processing_status,
+                    "user_id": "fake_user_id",
+                }
+            ),
+            "fake_topic",
+        )
+
+
+def test_subscribe(message_service_port: MessageServicePort):
+    async def fake_subscribe(topic: str):
+        pass
+
+    with patch.object(
+        adapter_instance,
+        "subscribe",
+        MagicMock(side_effect=fake_subscribe),
+    ) as mock_method:
+        asyncio.run(message_service_port.subscribe("fake_topic"))
+        mock_method.assert_called_once_with("fake_topic")
+
+
+def test_wait_for_message(message_service_port: MessageServicePort):
+    async def fake_wait_for_message(topic: str):
+        return json.dumps(
+            {
+                "inference_id": "fake_inference_id",
+                "output": 0.777,
+                "diagnosis": "positive",
+            }
+        )
+
+    with patch.object(
+        adapter_instance,
+        "wait_for_message",
+        MagicMock(side_effect=fake_wait_for_message),
+    ) as mock_method:
+        return_value = asyncio.run(message_service_port.wait_for_message("fake_topic"))
+        assert return_value == ResultUpdate(
+            **{
+                "inference_id": "fake_inference_id",
+                "output": 0.777,
+                "diagnosis": "positive",
+            }
+        )
