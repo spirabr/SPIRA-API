770c536ae03962ab6400824c70e9102b0d09644d
Vitor Guidi <vitorguidi@gmail.com>
2023-11-15 00:57:45 -0300

    Modifying init script to create all collections and initialize hospital

--- init.py
+++ init.py
@@ -9,6 +9,10 @@ password = os.environ["BOOTSTRAP_MONGO_PASSWORD"]
 email = "spira-root@nowhere.com"
 db_name = os.environ["DATABASE_NAME"]
 user_col= os.environ["user_collection_name"]
+model_col = os.environ["model_collection_name"]
+inf_col = os.environ["inference_collection_name"]
+results_col = os.environ["result_collection_name"]
+hosp_col = os.environ["hospital_collection_name"]
 conn_url=os.environ["M_CONN_URL"]
 
 def main():
@@ -19,6 +23,11 @@ def main():
     conn=MongoClient(conn_url)
     db=getattr(conn, db_name)
     users=getattr(db, user_col)
+    inferences=getattr(db, inf_col)
+    models=getattr(db, model_col)
+    results=getattr(db, results_col)
+    hospitals=getattr(db, hosp_col)
+
 
     entry = {
         "email": email,
@@ -26,9 +35,19 @@ def main():
         "username": username,
     }
 
+
     print("Attempting to crease user {}".format(entry["username"]), file=sys.stdout )
 
     users.insert_one(entry)
+    print("Success", file=sys.stdout)
+
+    print("Attempting to crease hospital {}".format(entry["hospital"]), file=sys.stdout )
+    hospital = {
+        "name": "einstein",
+        "id": whatever,
+    }
+    hospitals.insert_one(hospital)
+
     print("Success", file=sys.stdout)
 
 main()
